757a4f5409aab48cb525ae37e96754b3cbd042b6
RyanMather <76264545+RyanMather@users.noreply.github.com>
2021-09-21 12:15:55 -0500

    Question 3 Finished

--- PS2/JuliaCode/Compute_Draft1.jl
+++ PS2/JuliaCode/Compute_Draft1.jl
@@ -86,7 +86,7 @@ function V_iterate(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Flo
         err = abs.(maximum(v_next.-res.val_func))/abs(v_next[prim.na, 1]) #reset error level
         res.val_func = .8*v_next+.2*res.val_func #update value function
         n+=1
-        if mod(n,20)==0
+        if mod(n,50)==0
             println("Value Function iteration $(n), Error $(err)")
         end
     end
@@ -145,5 +145,28 @@ function Solve_model() #prim::Primitives, res::Results)
         Outer_loop_Iter+=1
     end
     return prim, res
+end
+
+
+#Get Distribution for Plotting
+function FindDist_ForPlot(prim,res; dist_tol::Float64 = 1e-6, dist_err::Float64 = 100.0,)
+    @unpack Π, na, a_grid= prim
+    TransMat=zeros(2*na,2*na) #The first na points are for employed folks, and the next na are for unemployed
+    for a_index=1:na
+        TransMat[Int64(res.pol_func[a_index,1]),a_index]=Π[1,1] #Savings choice for those moving from emp->emp
+        TransMat[Int64(res.pol_func[a_index,1])+na,a_index]=Π[2,1] #Savings choice for those moveing from emp->unemp
+        TransMat[Int64(res.pol_func[a_index,2]),a_index+na]=Π[1,2] #Savings choice for those moving from unemp->emp
+        TransMat[Int64(res.pol_func[a_index,2])+na,a_index+na]=Π[2,2] #Savings choice for those moving from unemp->emp
+    end
+    Dist=ones(2*na)*(1/(2*na)) #
+    Dist_new=copy(Dist)
+    while dist_err>dist_tol
+        for i=1:20  #Iterate until we reach the steady-state distribution
+            Dist_new=TransMat*Dist_new
+        end
+        dist_err=abs.(maximum(Dist_new.-Dist))
+        Dist=copy(Dist_new)
+    end
+    return Dist, Dist[1:na].+Dist[(na+1):2*na]
 end
 ##############################################################################
--- PS2/JuliaCode/RunCode_Draft1.jl
+++ PS2/JuliaCode/RunCode_Draft1.jl
@@ -1,4 +1,3 @@
-#using Plots, LaTeXStrings #import the libraries we want
 #Getting the Parellel Ready
     using Distributed #, SharedArrays
     #Re-initializing the workers
@@ -11,9 +10,18 @@
      #initialize primitive and results structs
     @time out_primitives, out_results = Solve_model() #solve the model!
     @unpack val_func, pol_func = out_results
-    @unpack a_grid = out_primitives
+    @unpack a_grid, na = out_primitives
 
 #Plot
+using Plots, LaTeXStrings #import the libraries we want
 Plots.plot(a_grid, val_func[:,1], title="Value Function", label="Employed")
     plot!(a_grid, val_func[:,2], label="Unemployed")
-#Plots.savefig("JuliaCode/Value_Functions.png")
+    Plots.savefig("Value_Functions.png")
+Plots.plot(a_grid, pol_func[:,1], title="Policy Functions", label="Employed")
+    plot!(a_grid, pol_func[:,2], label="Unemployed")
+    Plots.savefig("Policy_Functions.png")
+Type_Specific_Distribution, SS_Distribution=FindDist_ForPlot(out_primitives,out_results)
+    Plots.plot(a_grid, Type_Specific_Distribution[1:na], title="Value Function", label="Employed")
+        plot!(a_grid, Type_Specific_Distribution[(na+1):2*na], label="Unemployed")
+        Plots.savefig("Distribution.png")
+#
