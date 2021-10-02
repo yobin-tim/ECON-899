ae43b119c1a2c1d11c58a739d94c5e5776923d8a
RyanMather <76264545+RyanMather@users.noreply.github.com>
2021-10-02 13:54:56 -0500

    Additional Graph

--- PS3/JuliaCode/run_model.jl
+++ PS3/JuliaCode/run_model.jl
@@ -21,7 +21,7 @@ hcat(prim.a_grid, agridf, prim.a_grid - agridf)
 =#
 @time out_prim, out_res = MarketClearing(use_Fortran=false);
 
-using Plots
+using Plots, LaTeXStrings
 theme(:juno)
 plot(out_res.val_fun[:,:, end])
 plot!(out_res.val_fun[:,:, end-1])
@@ -48,8 +48,31 @@ savings = out_res.pol_fun[:,:,:] .- out_prim.a_grid;
 plot(out_prim.a_grid, savings[:,1,20])
 plot!(out_prim.a_grid, savings[:,2,20])
 
-plot(out_prim.a_grid, out_res.pol_fun[:,1,20])
-plot!(out_prim.a_grid, out_res.pol_fun[:,2,20])
+#Plotting Policy Functions
+    #Find a_hat (The asset point beyond which everyone dissaves)
+    function Find_a_hat()
+        a_hat=zeros(out_prim.nZ, out_prim.N_final)
+        for zi=1:out_prim.nZ, ni=1:out_prim.N_final
+            for ai=1:out_prim.nA
+                if out_res.pol_fun[ai,zi,ni]<=out_prim.a_grid[ai]
+                    a_hat[zi,ni]=out_prim.a_grid[ai]
+                    break
+                end
+            end
+        end
+        return a_hat
+    end
+    a_hat=Find_a_hat()
+    plot(out_prim.a_grid, out_res.pol_fun[:,1,20])
+    plot!(out_prim.a_grid, out_res.pol_fun[:,2,20])
+    vline!([a_hat[1,20]], label=L"\hat{a}")
+
+    plot(1:out_prim.N_final,a_hat[1,:],
+        xlabel="Age", ylabel=L"\hat{a}",label="High Schock")
+    plot!(1:out_prim.N_final,a_hat[2,:], title="Maximum Assets at which an Agent Saves
+over the Life Cycle",
+        xlabel="Age", ylabel=L"\hat{a}",label="Low Schock")
+
 plot!(out_prim.a_grid, out_prim.a_grid)
 
 plot(savings[500,1,:])
@@ -61,3 +84,5 @@ plot!
 # accross assets for workers and retirees
 a_dist = sum(out_res.F, dims = 2:3)
 plot(out_prim.a_grid, a_dist[:, 1, 1])
+
+plot(out_prim.a_grid,out_res.F[:,1,50])
