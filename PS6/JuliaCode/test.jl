c6f8518baed24e1ff116558a0cafb65fefe3afcb
Mitchell Valdes-Bobes <miche.v.b@gmail.com>
2021-10-20 15:47:51 -0500

    minor changes

---
+++ PS6/JuliaCode/test.jl
@@ -0,0 +1,20 @@
+include("./hopenhayn_rogerson.jl")
+
+prim, res = Initialize()
+
+market_clearing(prim, res)
+
+
+
+
+
+
+
+
+using Plots
+theme(:juno)
+
+plot(res.W_val)
+scatter!(res.W_val)
+plot(res.x_opt)
+scatter!(res.x_opt)
