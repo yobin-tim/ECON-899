b9a1e5a6a3472a7c59dbb77cb9a4fc273e8b2fa8
RyanMather <76264545+RyanMather@users.noreply.github.com>
2021-10-22 21:00:30 -0500

    Updates to Problem Set 7
    The Newey-West correction doesn't seem right yet

--- PS7/JuliaCode/Code.jl
+++ PS7/JuliaCode/Code.jl
@@ -9,6 +9,7 @@ using Parameters, Optim, Distributions, LinearAlgebra, Plots, LaTeXStrings
     gpoints ::Int64             = 10
     ρgrid   ::Array{Float64}    = collect(range(0.35, length = gpoints, stop = 0.65))
     σgrid   ::Array{Float64}    = collect(range(0.8, length = gpoints, stop = 1.2))
+    iT      ::Int64             = 4
 end # Primitives
 @with_kw mutable struct  Results
     b       ::Array{Float64} = [0,0]
@@ -64,6 +65,9 @@ function FindM2_MeanVar(data)
     end
     return M
 end
+function m_MeanVar(x,ind, mean)
+    return [x[ind]  (x[ind]-mean)^2]
+end
 function FindM2_VarCoVar(data)
     M=zeros(2)
     if size(data,2)>1 #We are dealing with the Model Data
@@ -80,38 +84,104 @@ function FindM2_VarCoVar(data)
     end
     return M
 end
-function J(g,W,b)
-    if b[2]<0
-        return 100
+function m_VarCovar(x,ind,mean)
+    return [(x[ind]-mean)^2      (x[ind]-mean)*(x[ind-1]-mean)]
+end
+function FindM3(data)
+    M=zeros(3)
+    if size(data,2)>1 #We are dealing with the Model Data
+        Hmeans=sum(data,dims=1)./size(data,1)
+        M[1]=sum(Hmeans)/size(data,2)
+        #data.-Hmeans does the subtraction for each row
+        Hvars=sum((data.-Hmeans).^2,dims=1)./size(data,1)
+        M[2]=sum(Hvars)/size(data,2)
+        HCovars=sum((data[2:end,:].-Hmeans).*(data[1:(end-1),:].-Hmeans),dims=1)./(size(data,1)-1)
+        M[3]=sum(HCovars)/size(data,2)
+    else #We are dealing with the true data
+        Mean=sum(data)/length(data)
+        M[1]=Mean;
+        M[2]=sum((data.-Mean).^2)/length(data)
+        M[3]=sum((data[2:end].-Mean).*(data[1:(end-1)].-Mean))/(length(data)-1)
+    end
+    return M
+end
+function m3(x,ind,mean)
+    [x[ind]   (x[ind]-mean)^2      (x[ind]-mean)*(x[ind-1]-mean)]
+end
+function J(g,W,b) #The objective function
+    if b[2]<0 #We shouldn't allow there to be a negative variance term
+        return 1e25
     else
         return transpose(g)*W*g
     end
 end
 
+function NeweyWest(prim::Primitives,simdata::Array{Float64},
+        m::Function, MTH::Array{Float64})
+    @unpack iT,H,T = prim
+    Hmeans=sum(simdata,dims=1)./size(simdata,1)
+    #Defining Γ function
+        function ΓjTH(j)
+            out=zeros(length(MTH),length(MTH))
+            for hi=1:H, ti=(j+1+1):T
+                #Necessary to add one twice above because one moment includes a lag
+                out+=(m(simdata[:,hi],ti,Hmeans[hi]).-MTH)*
+                    transpose(m(simdata[:,hi],ti-j,Hmeans[hi]).-MTH)
+            end
+            return (1/((T-1)*H))*out
+        end
+    #Finding STH
+        SyTH=ΓjTH(0)
+        for ji=1:iT
+            SyTH+=(1-ji/(iT+1))*(ΓjTH(ji)+transpose(ΓjTH(ji)))
+        end
+        STH=(1+1/H)*SyTH
+    #Returning WStar
+        return inv(STH)
+end
+
 function StepsAThroughD()
     prim=Primitives()
     e=eDrawsForModel(prim)
-    for Exercise=4:5
+    for Exercise=4:6
         if Exercise==4
-            FindM2=FindM2_MeanVar
+            FindM=FindM2_MeanVar
+            m=m_MeanVar
         elseif Exercise==5
-            FindM2=FindM2_VarCoVar
+            FindM=FindM2_VarCoVar
+            m=m_VarCovar
+        elseif Exercise==6
+            FindM=FindM3
+            m=m3
         end
         td=TrueData(prim)
 
-        #Part a: Graph in three Dimensions
-            @unpack ρgrid,σgrid,gpoints=prim
-            Jgrid=zeros(gpoints,gpoints)
-            for ρi=1:gpoints,σi=1:gpoints
-                md=ModelData(prim,e, [ρgrid[ρi] σgrid[σi]])
-                Jgrid[ρi,σi]=J(FindM2(td).-FindM2(md),I,[ρgrid[ρi] σgrid[σi]])
+        #Function for parts a and b
+            function GraphAndFindbHat(W; NeweyWest=false)
+                @unpack ρgrid,σgrid,gpoints=prim
+                Jgrid=zeros(gpoints,gpoints)
+                for ρi=1:gpoints,σi=1:gpoints
+                    md=ModelData(prim,e, [ρgrid[ρi] σgrid[σi]])
+                    Jgrid[ρi,σi]=J(FindM(td).-FindM(md),W,[ρgrid[ρi] σgrid[σi]])
+                end
+                Solution=optimize(b->J(FindM(td).-FindM(ModelData(prim,e,b)),W,b),
+                    [.3 1.2],NelderMead())
+                bHat=Solution.minimizer
+                plot(ρgrid,σgrid,Jgrid, st=:surface,
+                    title=L"\hat{b}^{1}_{TH}=[%$(round(bHat[1],digits=4)) , %$(round(bHat[2],digits=4)) ]")
+                if NeweyWest
+                    savefig("PS7\\Figures\\Exercise$(Exercise)NeweyWestCorrection.png")
+                else
+                    savefig("PS7\\Figures\\Exercise$(Exercise).png")
+                end
+                return bHat
             end
-            Solution=optimize(b->J(FindM2(td).-FindM2(ModelData(prim,e,b)),I,b),
-                [.3 1.2],NelderMead())
-            bHat1TH=Solution.minimizer
-            plot(ρgrid,σgrid,Jgrid, st=:surface,
-                title=L"\hat{b}^{1}_{TH}=[%$(round(bHat1TH[1],digits=4)) , %$(round(bHat1TH[2],digits=4)) ]")
-            savefig("PS7\\Figures\\Exercise$(Exercise).png")
-        #Part b: Find the
-    end
-end
+        #Part a: Graph in three Dimensions
+            bHat1TH=GraphAndFindbHat(I)
+        #Part b: Use NeweyWest to update your guess of bHat
+            md_bHat1TH=ModelData(prim,e, bHat1TH)
+            WStar=NeweyWest(prim,ModelData(prim,e, md_bHat1TH),
+                    m, FindM(md_bHat1TH))
+            bHat2TH=GraphAndFindbHat(WStar; NeweyWest=true)
+    end #Ending Loop over Exercises
+end #End Function StepsAThroughD
