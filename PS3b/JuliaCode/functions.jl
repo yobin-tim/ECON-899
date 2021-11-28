cb874b424874accdbca6fdf7d1c9d030ad3f5055
Mitchell Valdes-Bobes <miche.v.b@gmail.com>
2021-11-27 21:53:42 -0600

    Newton method returns NAN

--- PS3b/JuliaCode/functions.jl
+++ PS3b/JuliaCode/functions.jl
@@ -1,19 +1,26 @@
 # Load packages
-using Parameters
+using LinearAlgebra
 
 # Auxiliary functions
 # Choice probability function
 function choice_probability(δ::Array{Float64}, μ::Array{Float64}; eval_jacobian::Bool = false)
     
-    R = size(μ)[2];
-    Δ = exp.( δ .+ μ  )
-    Δ = Δ./(1 .+ sum(Δ, dims=1))
+    # number of individuals and choicesm
+    r, R = size(μ);
 
-    # if eval_jacobian
-        #return Δ, jacobian(Δ, δ)
-    # else
-        return sum(Δ, dims=2)/R
-    # end
+    # Compute choice probabilities
+    Λ = exp.( δ .+ μ  )
+    Λ = Λ./(1 .+ sum(Λ, dims=1))
+    σ = sum(Λ, dims=2)/R
+
+    if eval_jacobian
+        # Compute Jacobian
+        Δ = 1/R * ( I(r) .* (σ *(1 .- σ)') - (1 .- I(r)) .* (σ * σ') )
+        return σ, Δ
+    else
+        return σ   
+    end
+
 end
 
 # Model Structures
@@ -41,11 +48,12 @@ end
 
 
 # Demand inversion Contraction Mapping (Barry et al., 1995)
-function inverse_demand_cm(δ, s, μ, tol; max_iter::Int64 = 1000)
+function inverse_demand_cm(δ, s, μ, tol; max_iter::Int64 = 300)
 
     # Initial guess for the inverse demand
-    δ₀ = copy(δ )
-
+    δ₀ = copy( δ )
+    δ₁ = copy( δ )
+    
     # Initialize the iteration counter
     iter = 0
     # Initialize the error
@@ -57,13 +65,13 @@ function inverse_demand_cm(δ, s, μ, tol; max_iter::Int64 = 1000)
         σ = choice_probability(δ₀, μ)
 
         # Compute the inverse demand
-        δ = δ₀ + log.(s) - log.(σ)
+        δ₁ = δ₀ + log.(s) - log.(σ)
         
         # Update the error
-        err = maximum( abs.(δ - δ₀) )
-        
+        err = maximum( abs.(δ₁ - δ₀) )
+
         # Update the inverse demand
-        δ₀ = copy(δ)
+        δ₀ = copy(δ₁)
         
         # Update the iteration counter
         iter = iter + 1
@@ -71,8 +79,42 @@ function inverse_demand_cm(δ, s, μ, tol; max_iter::Int64 = 1000)
         @show err
     end
 
-    return δ
+    return δ₁
 end
+
+# Demand inversion Newtons Method
+function inverse_demand_nm(δ, s, μ, tol; max_iter::Int64 = 300)
+
+    # Initial guess for the inverse demand
+    δ₀ = copy( δ )
+    δ₁ = copy( δ )
+    # Initialize the iteration counter
+    iter = 0
+    # Initialize the error
+    err = 100
+
+    # Iterate until convergence
+    while (err > tol) && (iter < max_iter)
+        # Compute the choice probability and the Jacobian
+        σ, Δ = choice_probability(δ₀, μ, eval_jacobian=true)
+
+        # Compute the inverse demand
+        δ₁ = δ₀ - Δ^(-1) * (log.(s) - log.(σ))
+        
+        # Update the error
+        err = maximum( abs.(δ₁ - δ₀) )
+
+        # Update the inverse demand
+        δ₀ = copy(δ₁)
+        
+        # Update the iteration counter
+        iter = iter + 1
+        @show iter
+        @show err
+    end
+    return δ₁
+end
+
 
 # Demand inverter
 function inverse_demand(model::Model, year::Int64; method::String="Newton")
@@ -88,17 +130,26 @@ function inverse_demand(model::Model, year::Int64; method::String="Newton")
     # Get the inital guess for the inverse demand
     δ = [model.δ[year][j] for j in J]
 
+    # Compute the matrix μ[i,j] = λₚ * Y[i] * P[j]
     μ = repeat(Y', length(J), 1) .* P
 
-
-
+    # Compute the inverse demand
 
     if method=="Newton"
-
+        ε = 1e-12
+        ε₁ = 1
+        δ = inverse_demand_cm(δ, S, μ, ε₁)
+        δ = inverse_demand_nm(δ, S, μ, ε)
     elseif method=="CM"
-
+        ε = 1e-12
+        δ = inverse_demand_cm(δ, S, μ, ε)
     else
         error("Method not implemented, implemented methods are \"Newton\" and \"CM\" ")
+    end
+
+    # Update the inverse demand in the model
+    for j in 1:length(J)
+        model.δ[year][J[j]] = δ[j]
     end
 
 end
