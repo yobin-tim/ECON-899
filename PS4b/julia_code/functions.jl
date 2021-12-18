819e04481047c69d2928323689fbb29433dccd4a
dannyedgel <edgeldan@gmail.com>
2021-12-18 17:29:49 -0600

    finished problem 1 (but didn't write results to document

--- PS4b/julia_code/functions.jl
+++ PS4b/julia_code/functions.jl
@@ -1,13 +1,13 @@
 # Load packages
-using LinearAlgebra, Parameters, Optim, CSV
+using LinearAlgebra, Parameters, Optim, CSV, Printf
 
 ## Structure that contains the parameters of the model
 @with_kw mutable struct Primitives
-    λ::Int64 = -4
-    α::Int64 = 2
-    β::Float64 = 0.99
-    pᵣ::Int64 = 4
-    pₛ::Int64 = 1
+    λ₀::Int64   = -4
+    α::Int64    = 2
+    β::Float64  = 0.99
+    pᵣ::Int64   = 4
+    pₛ::Int64   = 1
     imax::Int64 = 8
 
     # Markov transition matrix
@@ -17,8 +17,8 @@ using LinearAlgebra, Parameters, Optim, CSV
     ]
 
     # Utility function
-    U::Function = (a, i, c, p, ϵ) -> (a == 1) ? α * c - p + ϵ[2] : (
-                        (i > 0) ? α * c + ϵ[1] : λ * (c > 0) + ϵ[0])
+    U::Function = (a, i, c, p, λ, ϵ) -> (a == 1) ? α * c - p + ϵ : (
+                        (i > 0) ? α * c + ϵ[1] : λ * (c > 0) + ϵ)
 
     # Parameters that must be loaded:
     # State space S 
@@ -31,3 +31,55 @@ using LinearAlgebra, Parameters, Optim, CSV
     F₁::Array{Float64,2} = (DataFrame(CSV.File(
         "./PS4b/ox_code/PS4_transition_a1.csv"))|>Matrix)[:, 3:end]
 end # Primitives struct 
+
+
+## Function calculates expected value
+function ExpVal(prim::Primitives, P, λ)
+
+    # unpack relevant primitives
+    @unpack α, β, S, U, F₀, F₁ = prim
+    γ = MathConstants.eulergamma;
+
+    # define utility levels for each state space and choice of a 
+    U₁ = U.(1, S[:, 1], S[:, 2], S[:, 3], λ, 0)
+    U₀ = U.(0, S[:, 1], S[:, 2], S[:, 3], λ, 0)
+
+    # steps from JF's code (not totally clear to me)
+    ϵ₀, ϵ₁ = γ .- log.(1 .- P), γ .- log.(P)
+    F = F₀.*(1 .- P) .+ F₁.*P
+
+    EU = (1 .- P).*(U₀ .+ ϵ₀) .+ P.*(U₁ .+ ϵ₁)
+    EV = inv(I(size(P, 1)) .- β.*F)*EU
+
+    V = [(U₀ + β*F₀*EV) (U₁ + β*F₁*EV)]
+
+    return EV, exp.(V[:, 2]) ./ sum(exp.(V), dims = 2)
+
+end # ExpVal()
+
+## Wrapper function for the expected value function that performs
+## the CCP algorithm
+function CCP(prim::Primitives, P₀; ε = 10e-10, err = 100, N = 100)
+
+    # unpack relevant primitives
+    @unpack λ₀ = prim
+
+    # initialize iteration counter
+    i = 1;
+
+    # iterate the expected value function until convergence
+    while err > ε
+        EV, P = ExpVal(prim, P₀, λ₀)
+        err = norm(P - P₀)
+
+        # print progress every N iterations
+        if (i % N == 0)
+            println(@sprintf "%.0f iterations, err = %.5f" i err)
+        end
+        i += 1; P₀ = P;
+    end # convergence loop
+
+    # return converged choice probability and expected value
+    return EV, P
+
+end # CCP()
--- PS4b/julia_code/runfile.jl
+++ PS4b/julia_code/runfile.jl
@@ -26,8 +26,24 @@ prim = Primitives()
 sim = (DataFrame(CSV.File("./PS4b/ox_code/PS4_simdata.csv")) 
         |> Matrix)[:, 2:end]
 
+
+
 #==
         Problem 1
     -------------------
 ==#
 
+# initial guess of P: 1/2 for all entries
+P₀ = .5*ones(size(prim.S, 1), 1)
+
+# Iterate until CCP convergence 
+EV, P = CCP(prim, P₀; N = 1);
+#==
+#TODO: figure out how to print vector to string
+# print the fixed-point EV
+fname = "./PS4b/document/Problem1.tex";
+open(fname, "w") do io
+    str = @sprintf "%.2f" EV
+    write(io, str)
+end;
+==#
